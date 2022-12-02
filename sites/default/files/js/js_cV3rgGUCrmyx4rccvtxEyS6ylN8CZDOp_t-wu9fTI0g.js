37d5f4461751ea35b3a774bc0c55d39e647510a7
Stan <st.takt@gmail.com>
2022-12-02 08:52:50 +0300

    initial config

---
+++ sites/default/files/js/js_cV3rgGUCrmyx4rccvtxEyS6ylN8CZDOp_t-wu9fTI0g.js
@@ -0,0 +1,457 @@
+/*!
+ * jQuery Once v2.2.3 - http://github.com/robloach/jquery-once
+ * @license MIT, GPL-2.0
+ *   http://opensource.org/licenses/MIT
+ *   http://opensource.org/licenses/GPL-2.0
+ */
+(function(e){"use strict";if(typeof exports==="object"&&typeof exports.nodeName!=="string"){e(require("jquery"))}else if(typeof define==="function"&&define.amd){define(["jquery"],e)}else{e(jQuery)}})(function(t){"use strict";var r=function(e){e=e||"once";if(typeof e!=="string"){throw new TypeError("The jQuery Once id parameter must be a string")}return e};t.fn.once=function(e){var n="jquery-once-"+r(e);return this.filter(function(){return t(this).data(n)!==true}).data(n,true)};t.fn.removeOnce=function(e){return this.findOnce(e).removeData("jquery-once-"+r(e))};t.fn.findOnce=function(e){var n="jquery-once-"+r(e);return this.filter(function(){return t(this).data(n)===true})}});
+
+/**
+ * @file
+ * auto_submit.js
+ *
+ * Provides a "form auto-submit" feature for the Better Exposed Filters module.
+ */
+
+(function ($, Drupal) {
+
+  /**
+   * To make a form auto submit, all you have to do is 3 things:.
+   *
+   * Use the "better_exposed_filters/auto_submit" js library.
+   *
+   * On gadgets you want to auto-submit when changed, add the
+   * data-bef-auto-submit attribute. With FAPI, add:
+   * @code
+   *  '#attributes' => array('data-bef-auto-submit' => ''),
+   * @endcode
+   *
+   * If you want to have auto-submit for every form element, add the
+   * data-bef-auto-submit-full-form to the form. With FAPI, add:
+   * @code
+   *   '#attributes' => array('data-bef-auto-submit-full-form' => ''),
+   * @endcode
+   *
+   * If you want to exclude a field from the bef-auto-submit-full-form auto
+   * submission, add an attribute of data-bef-auto-submit-exclude to the form
+   * element. With FAPI, add:
+   * @code
+   *   '#attributes' => array('data-bef-auto-submit-exclude' => ''),
+   * @endcode
+   *
+   * Finally, you have to identify which button you want clicked for autosubmit.
+   * The behavior of this button will be honored if it's ajaxy or not:
+   * @code
+   *  '#attributes' => array('data-bef-auto-submit-click' => ''),
+   * @endcode
+   *
+   * Currently only 'select', 'radio', 'checkbox' and 'textfield' types are
+   * supported. We probably could use additional support for HTML5 input types.
+   */
+  Drupal.behaviors.betterExposedFiltersAutoSubmit = {
+    attach: function (context) {
+      // When exposed as a block, the form #attributes are moved from the form
+      // to the block element, thus the second selector.
+      // @see \Drupal\block\BlockViewBuilder::preRender
+      var selectors = 'form[data-bef-auto-submit-full-form], [data-bef-auto-submit-full-form] form, [data-bef-auto-submit]';
+
+      // The change event bubbles so we only need to bind it to the outer form
+      // in case of a full form, or a single element when specified explicitly.
+      $(selectors, context).addBack(selectors).each(function (i, e) {
+        // Store the current form.
+        var $form = $(e);
+
+        // Retrieve the autosubmit delay for this particular form.
+        var autoSubmitDelay = $form.data('bef-auto-submit-delay') || 500;
+
+        // Attach event listeners.
+        $form.once('bef-auto-submit')
+          // On change, trigger the submit immediately.
+          .on('change', triggerSubmit)
+          // On keyup, wait for a specified number of milliseconds before
+          // triggering autosubmit. Each new keyup event resets the timer.
+          .on('keyup', Drupal.debounce(triggerSubmit, autoSubmitDelay));
+      });
+
+      /**
+       * Triggers form autosubmit when conditions are right.
+       *
+       * - Checks first that the element that was the target of the triggering
+       *   event is `:text` or `textarea`, but is not `.hasDatePicker`.
+       * - Checks that the keycode of the keyup was not in the list of ignored
+       *   keys (navigation keys etc).
+       *
+       * @param {object} e - The triggering event.
+       */
+      function triggerSubmit(e) {
+        // e.keyCode: key.
+        var ignoredKeyCodes = [
+          16, // Shift.
+          17, // Ctrl.
+          18, // Alt.
+          20, // Caps lock.
+          33, // Page up.
+          34, // Page down.
+          35, // End.
+          36, // Home.
+          37, // Left arrow.
+          38, // Up arrow.
+          39, // Right arrow.
+          40, // Down arrow.
+          9, // Tab.
+          13, // Enter.
+          27  // Esc.
+        ];
+
+        // Triggering element.
+        var $target = $(e.target);
+        var $submit = $target.closest('form').find('[data-bef-auto-submit-click]');
+
+        // Don't submit on changes to excluded elements or a submit element.
+        if ($target.is('[data-bef-auto-submit-exclude], :submit') || ($target.attr('autocomplete') == 'off' && !$target.hasClass('bef-datepicker'))) {
+          return true;
+        }
+
+        // Submit only if this is a non-datepicker textfield and if the
+        // incoming keycode is not one of the excluded values.
+        if (
+          $target.is(':text:not(.hasDatepicker), textarea')
+          && $.inArray(e.keyCode, ignoredKeyCodes) === -1
+        ) {
+          $submit.click();
+        }
+        // Only trigger submit if a change was the trigger (no keyup).
+        else if (e.type === 'change') {
+          $submit.click();
+        }
+      }
+    }
+  }
+
+}(jQuery, Drupal));
+;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal, debounce) {
+  $.fn.drupalGetSummary = function () {
+    var callback = this.data('summaryCallback');
+    return this[0] && callback ? callback(this[0]).trim() : '';
+  };
+
+  $.fn.drupalSetSummary = function (callback) {
+    var self = this;
+
+    if (typeof callback !== 'function') {
+      var val = callback;
+
+      callback = function callback() {
+        return val;
+      };
+    }
+
+    return this.data('summaryCallback', callback).off('formUpdated.summary').on('formUpdated.summary', function () {
+      self.trigger('summaryUpdated');
+    }).trigger('summaryUpdated');
+  };
+
+  Drupal.behaviors.formSingleSubmit = {
+    attach: function attach() {
+      function onFormSubmit(e) {
+        var $form = $(e.currentTarget);
+        var formValues = $form.serialize();
+        var previousValues = $form.attr('data-drupal-form-submit-last');
+
+        if (previousValues === formValues) {
+          e.preventDefault();
+        } else {
+          $form.attr('data-drupal-form-submit-last', formValues);
+        }
+      }
+
+      $(once('form-single-submit', 'body')).on('submit.singleSubmit', 'form:not([method~="GET"])', onFormSubmit);
+    }
+  };
+
+  function triggerFormUpdated(element) {
+    $(element).trigger('formUpdated');
+  }
+
+  function fieldsList(form) {
+    return [].map.call(form.querySelectorAll('[name][id]'), function (el) {
+      return el.id;
+    });
+  }
+
+  Drupal.behaviors.formUpdated = {
+    attach: function attach(context) {
+      var $context = $(context);
+      var contextIsForm = $context.is('form');
+      var $forms = $(once('form-updated', contextIsForm ? $context : $context.find('form')));
+      var formFields;
+
+      if ($forms.length) {
+        $.makeArray($forms).forEach(function (form) {
+          var events = 'change.formUpdated input.formUpdated ';
+          var eventHandler = debounce(function (event) {
+            triggerFormUpdated(event.target);
+          }, 300);
+          formFields = fieldsList(form).join(',');
+          form.setAttribute('data-drupal-form-fields', formFields);
+          $(form).on(events, eventHandler);
+        });
+      }
+
+      if (contextIsForm) {
+        formFields = fieldsList(context).join(',');
+        var currentFields = $(context).attr('data-drupal-form-fields');
+
+        if (formFields !== currentFields) {
+          triggerFormUpdated(context);
+        }
+      }
+    },
+    detach: function detach(context, settings, trigger) {
+      var $context = $(context);
+      var contextIsForm = $context.is('form');
+
+      if (trigger === 'unload') {
+        once.remove('form-updated', contextIsForm ? $context : $context.find('form')).forEach(function (form) {
+          form.removeAttribute('data-drupal-form-fields');
+          $(form).off('.formUpdated');
+        });
+      }
+    }
+  };
+  Drupal.behaviors.fillUserInfoFromBrowser = {
+    attach: function attach(context, settings) {
+      var userInfo = ['name', 'mail', 'homepage'];
+      var $forms = $(once('user-info-from-browser', '[data-user-info-from-browser]'));
+
+      if ($forms.length) {
+        userInfo.forEach(function (info) {
+          var $element = $forms.find("[name=".concat(info, "]"));
+          var browserData = localStorage.getItem("Drupal.visitor.".concat(info));
+
+          if (!$element.length) {
+            return;
+          }
+
+          var emptyValue = $element[0].value === '';
+          var defaultValue = $element.attr('data-drupal-default-value') === $element[0].value;
+
+          if (browserData && (emptyValue || defaultValue)) {
+            $element.each(function (index, item) {
+              item.value = browserData;
+            });
+          }
+        });
+      }
+
+      $forms.on('submit', function () {
+        userInfo.forEach(function (info) {
+          var $element = $forms.find("[name=".concat(info, "]"));
+
+          if ($element.length) {
+            localStorage.setItem("Drupal.visitor.".concat(info), $element[0].value);
+          }
+        });
+      });
+    }
+  };
+
+  var handleFragmentLinkClickOrHashChange = function handleFragmentLinkClickOrHashChange(e) {
+    var url;
+
+    if (e.type === 'click') {
+      url = e.currentTarget.location ? e.currentTarget.location : e.currentTarget;
+    } else {
+      url = window.location;
+    }
+
+    var hash = url.hash.substr(1);
+
+    if (hash) {
+      var $target = $("#".concat(hash));
+      $('body').trigger('formFragmentLinkClickOrHashChange', [$target]);
+      setTimeout(function () {
+        return $target.trigger('focus');
+      }, 300);
+    }
+  };
+
+  var debouncedHandleFragmentLinkClickOrHashChange = debounce(handleFragmentLinkClickOrHashChange, 300, true);
+  $(window).on('hashchange.form-fragment', debouncedHandleFragmentLinkClickOrHashChange);
+  $(document).on('click.form-fragment', 'a[href*="#"]', debouncedHandleFragmentLinkClickOrHashChange);
+})(jQuery, Drupal, Drupal.debounce);;
+/**
+ * @file
+ * Extends methods from core/misc/form.js.
+ */
+
+(function ($, window, Drupal, drupalSettings) {
+
+  /**
+   * Behavior for "forms_has_error_value_toggle" theme setting.
+   */
+  Drupal.behaviors.bootstrapForm = {
+    attach: function (context) {
+      if (drupalSettings.bootstrap && drupalSettings.bootstrap.forms_has_error_value_toggle) {
+        var $context = $(context);
+        $context.find('.form-item.has-error:not(.form-type-password.has-feedback)').once('error').each(function () {
+          var $formItem = $(this);
+          var $input = $formItem.find(':input');
+          $input.on('keyup focus blur', function () {
+            if (this.defaultValue !== void 0) {
+              $formItem[this.defaultValue !== this.value ? 'removeClass' : 'addClass']('has-error');
+              $input[this.defaultValue !== this.value ? 'removeClass' : 'addClass']('error');
+            }
+          });
+        });
+      }
+    }
+  };
+
+
+})(jQuery, this, Drupal, drupalSettings);
+;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal) {
+  function DetailsSummarizedContent(node) {
+    this.$node = $(node);
+    this.setupSummary();
+  }
+
+  $.extend(DetailsSummarizedContent, {
+    instances: []
+  });
+  $.extend(DetailsSummarizedContent.prototype, {
+    setupSummary: function setupSummary() {
+      this.$detailsSummarizedContentWrapper = $(Drupal.theme('detailsSummarizedContentWrapper'));
+      this.$node.on('summaryUpdated', $.proxy(this.onSummaryUpdated, this)).trigger('summaryUpdated').find('> summary').append(this.$detailsSummarizedContentWrapper);
+    },
+    onSummaryUpdated: function onSummaryUpdated() {
+      var text = this.$node.drupalGetSummary();
+      this.$detailsSummarizedContentWrapper.html(Drupal.theme('detailsSummarizedContentText', text));
+    }
+  });
+  Drupal.behaviors.detailsSummary = {
+    attach: function attach(context) {
+      DetailsSummarizedContent.instances = DetailsSummarizedContent.instances.concat(once('details', 'details', context).map(function (details) {
+        return new DetailsSummarizedContent(details);
+      }));
+    }
+  };
+  Drupal.DetailsSummarizedContent = DetailsSummarizedContent;
+
+  Drupal.theme.detailsSummarizedContentWrapper = function () {
+    return "<span class=\"summary\"></span>";
+  };
+
+  Drupal.theme.detailsSummarizedContentText = function (text) {
+    return text ? " (".concat(text, ")") : '';
+  };
+})(jQuery, Drupal);;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Drupal) {
+  Drupal.behaviors.detailsAria = {
+    attach: function attach() {
+      $(once('detailsAria', 'body')).on('click.detailsAria', 'summary', function (event) {
+        var $summary = $(event.currentTarget);
+        var open = $(event.currentTarget.parentNode).attr('open') === 'open' ? 'false' : 'true';
+        $summary.attr({
+          'aria-expanded': open,
+          'aria-pressed': open
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);;
+/**
+* DO NOT EDIT THIS FILE.
+* See the following change record for more information,
+* https://www.drupal.org/node/2815083
+* @preserve
+**/
+
+(function ($, Modernizr, Drupal) {
+  function CollapsibleDetails(node) {
+    this.$node = $(node);
+    this.$node.data('details', this);
+    var anchor = window.location.hash && window.location.hash !== '#' ? ", ".concat(window.location.hash) : '';
+
+    if (this.$node.find(".error".concat(anchor)).length) {
+      this.$node.attr('open', true);
+    }
+
+    this.setupSummaryPolyfill();
+  }
+
+  $.extend(CollapsibleDetails, {
+    instances: []
+  });
+  $.extend(CollapsibleDetails.prototype, {
+    setupSummaryPolyfill: function setupSummaryPolyfill() {
+      var $summary = this.$node.find('> summary');
+      $summary.attr('tabindex', '-1');
+      $('<span class="details-summary-prefix visually-hidden"></span>').append(this.$node.attr('open') ? Drupal.t('Hide') : Drupal.t('Show')).prependTo($summary).after(document.createTextNode(' '));
+      $('<a class="details-title"></a>').attr('href', "#".concat(this.$node.attr('id'))).prepend($summary.contents()).appendTo($summary);
+      $summary.append(this.$summary).on('click', $.proxy(this.onSummaryClick, this));
+    },
+    onSummaryClick: function onSummaryClick(e) {
+      this.toggle();
+      e.preventDefault();
+    },
+    toggle: function toggle() {
+      var _this = this;
+
+      var isOpen = !!this.$node.attr('open');
+      var $summaryPrefix = this.$node.find('> summary span.details-summary-prefix');
+
+      if (isOpen) {
+        $summaryPrefix.html(Drupal.t('Show'));
+      } else {
+        $summaryPrefix.html(Drupal.t('Hide'));
+      }
+
+      setTimeout(function () {
+        _this.$node.attr('open', !isOpen);
+      }, 0);
+    }
+  });
+  Drupal.behaviors.collapse = {
+    attach: function attach(context) {
+      if (Modernizr.details) {
+        return;
+      }
+
+      once('collapse', 'details', context).forEach(function (detail) {
+        detail.classList.add('collapse-processed');
+        CollapsibleDetails.instances.push(new CollapsibleDetails(detail));
+      });
+    }
+  };
+
+  var handleFragmentLinkClickOrHashChange = function handleFragmentLinkClickOrHashChange(e, $target) {
+    $target.parents('details').not('[open]').find('> summary').trigger('click');
+  };
+
+  $('body').on('formFragmentLinkClickOrHashChange.details', handleFragmentLinkClickOrHashChange);
+  Drupal.CollapsibleDetails = CollapsibleDetails;
+})(jQuery, Modernizr, Drupal);;
